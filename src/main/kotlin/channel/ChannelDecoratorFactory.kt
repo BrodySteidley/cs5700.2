8a09300aa9e7cbc60208c3f8156a4ed9e11b21bf
Brody <brody@steidley.org>
2026-07-09 18:50:45 -0600

    Improve error handling in channel decorator factory

--- src/main/kotlin/channel/ChannelDecoratorFactory.kt
+++ src/main/kotlin/channel/ChannelDecoratorFactory.kt
@@ -9,12 +9,26 @@ object ChannelDecoratorFactory {
         if (effect.size < 2)
             throw EffectParseException("no parameter")
 
-        return when (effect[0]) {
-            "vol" -> VolumeDecorator(channel, effect[1].toDouble())
-            "ads" -> if (effect.size != 4) throw EffectParseException("invalid ADS parameter count") else ADSEnvelopeDecorator(channel, effect[1].toDouble(), effect[2].toDouble(), effect[3].toDouble())
-            "tanh" -> TanhDistortionDecorator(channel, effect[1].toDouble())
-            "clip" -> ClipDistortionDecorator(channel, effect[1].toDouble())
-            else -> throw EffectParseException("Unknown effect name")
+        try {
+            return when (effect[0]) {
+                "vol" -> VolumeDecorator(channel, effect[1].toDouble())
+                "ads" -> if (effect.size != 4) throw EffectParseException("invalid ADS parameter count") else ADSEnvelopeDecorator(
+                    channel,
+                    effect[1].toDouble(),
+                    effect[2].toDouble(),
+                    effect[3].toDouble()
+                )
+
+                "tanh" -> TanhDistortionDecorator(channel, effect[1].toDouble())
+                "clip" -> ClipDistortionDecorator(channel, effect[1].toDouble())
+                else -> throw EffectParseException("Unknown effect name")
+            }
+        } catch (e : Exception) {
+            if (e !is EffectParseException) {
+                throw EffectParseException("Invalid parameters: $effectDescriptor")
+            } else {
+                throw e
+            }
         }
     }
 }
